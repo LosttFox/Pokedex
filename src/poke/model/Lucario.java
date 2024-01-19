63a118004bde13da453510629481ff0273e26cf7
LostFox <51424369+LosttFox@users.noreply.github.com>
2024-01-19 10:57:34 -0700

    added missing Pokémon classes

---
+++ src/poke/model/Lucario.java
@@ -0,0 +1,12 @@
+package poke.model;
+
+public class Lucario extends Pokemon
+{
+
+	public Lucario(int pokedexNumber, String name)
+	{
+		super(pokedexNumber, name);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/poke/model/Lugia.java
@@ -0,0 +1,12 @@
+package poke.model;
+
+public class Lugia extends Pokemon
+{
+
+	public Lugia(int pokedexNumber, String name)
+	{
+		super(pokedexNumber, name);
+		// TODO Auto-generated constructor stub
+	}
+
+}
