1242ec8d67459fdcd0c3a6cf54e07609451e7fac
LostFox <51424369+LosttFox@users.noreply.github.com>
2024-01-19 11:42:34 -0700

    Added the Pokémon Meowstic

---
+++ src/poke/model/Meowstic.java
@@ -0,0 +1,20 @@
+package poke.model;
+
+public class Meowstic extends Pokemon
+{
+
+	public Meowstic()
+	{
+		super(678, "Meowstic");
+	}
+	
+	public Meowstic(String name)
+	{
+		super(678, name);
+	}
+	
+	public Meowstic(int number, String name)
+	{
+		super(number, name);
+	}
+}
