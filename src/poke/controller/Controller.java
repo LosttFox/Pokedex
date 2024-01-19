327322bd784b81ce210f44c093cc14889f288b37
LostFox <51424369+LosttFox@users.noreply.github.com>
2024-01-19 15:07:07 -0700

    made a method to create pokedex with all pokemon

--- src/poke/controller/Controller.java
+++ src/poke/controller/Controller.java
@@ -1,14 +1,47 @@
 package poke.controller;
 
+import poke.model.*;
+import java.util.ArrayList;
+
 public class Controller
 {
+	private ArrayList<Pokemon> pokedex;
+	private String dataFile;
+	
 	public Controller()
 	{
+		this.pokedex = new ArrayList<Pokemon>();
+		this.dataFile = "";
 		
+		createPokedex();
 	}
 	
 	public void start()
 	{
 		
+	}
+	
+	private void createPokedex()
+	{
+		pokedex.add(new Braixen());
+		pokedex.add(new Cinderace());
+		pokedex.add(new Eevee());
+		pokedex.add(new Espurr());
+		pokedex.add(new Fennekin());
+		pokedex.add(new Flareon());
+		pokedex.add(new Floragato());
+		pokedex.add(new Lucario());
+		pokedex.add(new Lugia());
+		pokedex.add(new Meowscarada());
+		pokedex.add(new Meowstic());
+		pokedex.add(new Mew());
+		pokedex.add(new Nickit());
+		pokedex.add(new Ninetales());
+		pokedex.add(new Riolu());
+		pokedex.add(new Sprigatito());
+		pokedex.add(new Sylveon());
+		pokedex.add(new Thievul());
+		pokedex.add(new Vulpix());
+		pokedex.add(new Zorua());		
 	}
 }
