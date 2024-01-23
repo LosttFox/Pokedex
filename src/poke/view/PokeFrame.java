4a5c6bd705f3f6534978ef142cf872f9700ee70e
LostFox <51424369+LosttFox@users.noreply.github.com>
2024-01-23 12:47:23 -0700

    added PokePanel into PokeFrame

--- src/poke/view/PokeFrame.java
+++ src/poke/view/PokeFrame.java
@@ -6,18 +6,21 @@ import poke.controller.Controller;
 public class PokeFrame extends JFrame
 {
 	private Controller app;
+	private PokePanel window;
 	
 	public PokeFrame(Controller app)
 	{
 		super();
 		
 		this.app = app;
+		this.window = new PokePanel(this.app);
 		
 		setupFrame();
 	}
 	
 	private void setupFrame()
 	{
+		this.setContentPane(window);
 		this.setTitle("Pokédex");
 		this.setSize(1024, 768);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
