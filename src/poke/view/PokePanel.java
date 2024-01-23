3223d7a417ddc1edf48dc9061de9e57416877b36
LostFox <51424369+LosttFox@users.noreply.github.com>
2024-01-23 12:45:44 -0700

    created PokePanel class stub

---
+++ src/poke/view/PokePanel.java
@@ -0,0 +1,40 @@
+package poke.view;
+
+import javax.swing.*;
+import poke.controller.Controller;
+import java.awt.Color;
+import java.awt.GridLayout;
+
+public class PokePanel extends JPanel
+{
+	private Controller app;
+	
+	public PokePanel(Controller app)
+	{
+		super();
+		
+		this.app = app;
+		
+		
+		
+		setupPanel();
+		setupListeners();
+		setupLayout();
+	}
+	
+	private void setupPanel()
+	{
+		
+	}
+	
+	private void setupListeners()
+	{
+		
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+
+}
