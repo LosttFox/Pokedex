4d5a42a38e3f0dfd586ff5e208ab4ddb89942103
LostFox <51424369+LosttFox@users.noreply.github.com>
2024-01-31 12:32:41 -0700

    created IOController class with necessary imports

---
+++ src/poke/controller/IOController.java
@@ -0,0 +1,22 @@
+package poke.controller;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.IOException;
+
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+import poke.model.Pokemon;
+
+
+public class IOController
+{
+
+	public IOController()
+	{
+		
+	}
+
+}
